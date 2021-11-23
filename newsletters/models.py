293886210a0631ad531b8eb8cf69e651508575b5
Alex Aguiñaga <marioalejandro300402@gmail.com>
2021-11-22 21:02:44 -0600

    models: newsletter add tags

--- newsletters/models.py
+++ newsletters/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from tags.models import Tag
+
 class Newsletter(models.Model):
   name = models.CharField(max_length=100, default='dummy text')
   description = models.CharField(max_length=400, default='dummy description')
@@ -7,6 +9,7 @@ class Newsletter(models.Model):
   # target  <- no se que tipo de dato deba usarse
   # frecuency
   # created_by = models.ForeignKey(User)
+  tags = models.ManyToManyField(Tag, related_name='tags')
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now_add=True)
 
