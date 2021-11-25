b147ca803995616b3dfe97e9522f8205c6dd24d1
Alex Aguiñaga <marioalejandro300402@gmail.com>
2021-11-25 13:10:07 -0600

    merge: users models

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from users.models import User
-from users.serializers import UserSerializer
 #Serializers:
+from users.serializers import UserSerializer
 
 class UsersViewSet(ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
+  permission_classes = (IsAuthenticatedOrReadOnly)
